46754f221f64049a26a44089aed3b3c2feb95b44
Harry Sweet <hsweet52@gmail.com>
2016-12-21 07:46:38 -0500

    Update textrandom.py
    a beginning of some actions when player goes to a room

--- textrandom.py
+++ textrandom.py
@@ -1,14 +1,24 @@
 #imports the methods into this namespace, less typing
 from player import *
 
-def r1(player):
-    print ('You are sitting in a large dim room')
-    
-def r2(player):
-    pass
+def switchboard(player,room):
+    print ('You are in room {}'.format(room))
+    if room == 1:
+        print ('This is not your day')
+    elif room == 2:
+        pass
+    if 'chalk' in player.things:
+        print ('Consider this Python question carefully, O seeker of Truth')
+        answer = input ('What is "2" * 2?')
+        if answer == '22':      # input returns a string, not a number!
+            print ("You have answered correctly, Oh wise one and the powers have added a life")
+            player.lifes = player.lifes = + 1
+        else:
+            print ("The number you seek is 22, not 4!  The magic is the quotes around the first number")
+            player.lifes =  player.lifes = - 1
+    print (player.lifes)
+        
 
-def r8(player):
-    print ('Whoah ...')
 
 ###########################  Setup game here ###########
 
@@ -21,16 +31,22 @@ for line in open("rooms.csv"):
     room = line.split(",")          # single room list
     roomslist.append(room)          # list ofall the room lists
 # print (roomslist) 
+
+
 ############### ############Start a player ######################
 player = Player('Arlin', 1)
-# room = player.change_room()     
+#room = player.change_room()     
 
 ############################ Game Loop #########################
 while True:
     print (player.info())
     room = player.change_room()
+    switchboard(player, room)
     rm_name, rm_describe, rm_item = roomslist[room][0], roomslist[room][1],\
     roomslist[room][2]
     print ('{}, You are in {}, {}'.format(player.name, rm_name,rm_describe))  
-    print ('There is a {} here'.format(rm_item))
+    print ('There is a {} here - you take it.'.format(rm_item))
+    player.things.append("{}".format(rm_item))
+    print('Your inventory is now:')
+    print(player.inventory())
     input("pause")
